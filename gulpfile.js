810d421672748a1305b2ee481e62362ea1209533
Alex Somerville <j.alex.somerville@gmail.com>
2015-04-03 19:14:37 -0400

    Gulp task runner set up, basic styling automation run gulp watch

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var package = require('./package.json');
+
+var options = {};
+options.sass = {
+    errLogToConsole: true,
+    sourceMap: 'sass',
+    sourceComments: 'map',
+    style: 'compressed',
+    precision: 10,
+    imagePath: '/img',
+    includePaths: [
+    'scss/*.scss',
+    ]
+};
+options.autoprefixer = {
+    map: true,
+    from: 'asset',
+    to: 'asrp.min.css'
+};
+
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    livereload = require('gulp-livereload');
+
+gulp.task('js', function () {
+    return gulp.src('js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(uglify())
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('sass', function () {
+    gulp.src('scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass(options.sass))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', options.autoprefixer))
+    .pipe(gulp.dest('./css'))
+    .pipe(minifycss())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./css'))
+    .pipe(livereload())
+    ;
+});
+
+gulp.task('watch', function () {
+    livereload.listen();
+    gulp.watch('scss/**/*.scss', ['sass']);
+});
+
+gulp.on('err', function(err){
+    console.log(err);
+});
